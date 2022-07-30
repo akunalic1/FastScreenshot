f36babadbaf4941b04d2accd7fdcbe8d6910cd44
akunalic1 <72765733+akunalic1@users.noreply.github.com>
2022-07-30 17:32:33 +0200

    adding folders on post

--- backend/routes/Folders.js
+++ backend/routes/Folders.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const { Folders } = require("../models");
 
 router.route("/:id").get((req, res) => {
   console.log(req.params);
   res.send({ folder: "folder bre" });
 });
 
-router.route("/").get((req, res) => {
-  res.send({ message: "henlooo" });
-});
+router
+  .route("/")
+  .get((req, res) => {
+    res.send({ message: "henlooo" });
+  })
+  .post(async (req, res) => {
+    const data = req.body;
+    console.log("ono sto je poslano", data);
+    await Folders.create(data);
+    res.json(data);
+  });
 
 module.exports = router;
