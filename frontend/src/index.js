14bd0fc85fbe7c73cd36db9ec615eef4bf99f622
akunalic1 <72765733+akunalic1@users.noreply.github.com>
2022-08-28 18:40:29 +0200

    Removed router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,6 @@ import App from "./components/App.jsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
